937e6d15092899dfa46ac8a05805735e2f66fbff
zkitefly <z18344203426@qq.com>
2024-02-01 08:19:07 +0800

    add check if the download link is valid

--- main.py
+++ main.py
@@ -13,9 +13,14 @@ def fetch_file(file_name):
     text = response.text
     
     start = text.find("href='", text.find("<div class=\"downloadButton\">")) + len("href='")
-    download_link = text[start:text.find("'", start)]
+    download_link = "https://optifine.net/" + text[start:text.find("'", start)]
 
-    return redirect(f"https://optifine.net/{download_link}")
+    # Check if the download link is valid
+    download_response = httpx.head(download_link)
+    if download_response.status_code != 200 or 'Content-Disposition' not in download_response.headers:
+        return jsonify(message="File not found"), 404
+
+    return redirect(download_link)
 
 if __name__ == "__main__":
     app.run()
